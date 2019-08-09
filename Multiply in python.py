d0bd17976392122a03f3a51f76bfe50c19cbbb6a
shuvomoybasak <53864439+shuvomoybasak@users.noreply.github.com>
2019-08-09 20:03:45 +0530

    Add files via upload

---
+++ Multiply in python.py
@@ -0,0 +1,13 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+first_number = int(input("Enter your first number : "))
+second_number = int(input("Enter your second number : "))
+final_result = first_number * second_number
+print("Multiplication of {} and {} is : {}".format(first_number,second_number,final_result))
+
+
+# #### 
